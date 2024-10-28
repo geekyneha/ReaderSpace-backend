3d549ef877c5517144daf899159043c1e0e3ceb8
Neha <nehatualea@gmail.com>
2024-10-28 19:58:02 +0530

    Created: boks.js to save books

---
+++ books.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    "title": "To Kill a Mockingbird",
+    "author": "Harper Lee",
+    "genre": "Fiction",
+    "coverImage": "https://media.glamour.com/photos/56e1f3c562b398fa64cbd310/master/w_1600%2Cc_limit/entertainment-2016-02-07-main.jpg"
+  },
+  {
+    "title": "1984",
+    "author": "George Orwell",
+    "genre": "Dystopian",
+    "coverImage": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRue5i7Tq64O8WH3ECvx6BEt_dyUtMfHodWRA&s"
+  },
+  {
+    "title": "The magic",
+    "author": "hoolen",
+    "genre": "Sci-Fic",
+    "coverImage": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTn37hPwgAZfdLCP-ZjQAimvTC8xN0HgdfKDL_NoA_Kk3n5AWb31EcEBH_YcRXcXkD5FH8TErVzIXOOSS7kZOx0ghIOED0X1bRjSF5Pwi8&usqp=CAE"
+  }
+];
